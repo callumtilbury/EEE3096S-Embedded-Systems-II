ed6b585111764ee3ffa949b874beaca5ea9646e1
Callum Tilbury <ctilbury8@gmail.com>
2019-07-21 17:23:08 +0100

    Outputs to LEDs working in simple loop.

--- prac1_tlbcal002.py
+++ prac1_tlbcal002.py
@@ -9,33 +9,38 @@ Date: 21/07/2019
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
 
-# Logic that you write
-def main():
-	setup();
-	loop();
+# Variables:
+leds = [11, 13, 15]
 
 def setup():
-	GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+	# Use BOARD numbering
+	GPIO.setmode(GPIO.BOARD)
+	# Turn off warning messages
+	GPIO.setwarnings(False)
+	# Set up LED pins as outputs, and turn them off
+	GPIO.setup(leds, GPIO.OUT, initial=GPIO.LOW)
 
 def loop():
-	GPIO.output(11, 1)
-	time.sleep(1)
-	GPIO.output(11, 0)
-	time.sleep(1)
+	for i in range(3):
+		GPIO.output(leds[i],1)
+		time.sleep(1)
+		GPIO.output(leds[i],0)
 
 
 # Only run the functions if 
 if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
-    try:
-        while True:
-            main()
-    except KeyboardInterrupt:
-        print("Exiting gracefully")
-        # Turn off your GPIOs here
-        GPIO.cleanup()
-    except e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
+	# Make sure the GPIO is stopped correctly
+	try:
+		setup()
+		while True:
+			loop()
+	except KeyboardInterrupt:
+		print("Exiting gracefully")
+		# Turn off your GPIOs here
+		GPIO.cleanup()
+	except e:
+		GPIO.cleanup()
+		print("Some other error occurred")
+		print(e.message)
