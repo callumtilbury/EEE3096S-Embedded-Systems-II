77f30feee72c72eb7802cdc1405302faf2368d04
Callum Tilbury <ctilbury8@gmail.com>
2019-08-05 20:23:07 +0200

    C accuracy check is implemented, but there is an issue with number that it is outputing.

--- Prac2/C/src/Prac2.c
+++ Prac2/C/src/Prac2.c
@@ -5,12 +5,14 @@ extern float carrier[SAMPLE_COUNT];
 
 float result [SAMPLE_COUNT];
 
+float epsilon = 0.005;
+
 FILE *fptr;
 FILE *fptr_acc;
 
 int main(int argc, char**argv){
     fptr = fopen("../log.csv","a+");
-    fptr_acc = fopen("../GoldenMeasureData.csv","r+")
+    fptr_acc = fopen("../GoldenMeasureData.csv","r+");
 
     fprintf(fptr, "C,");
     printf("Running Unthreaded Test\n");
@@ -18,17 +20,31 @@ int main(int argc, char**argv){
     printf("Precision sizeof %d\n", sizeof(float));
     fprintf(fptr, "%d,",sizeof(float));
 
-    float correct_result = 0;
     tic(); // start the timer
     for (int i = 0;i<SAMPLE_COUNT;i++ ){
         result[i] = data[i] * carrier[i];
-        fscanf(inFile, "%f,", correct_result);
-	printf("%f\n",correct_result-result[i]);
     }
     double t = toc();
     printf("Time: %lf ms\n",t/1e-3);
-    fprintf(fptr, "%lf\n",t/1e-3);
+    fprintf(fptr, "%lf,",t/1e-3);
+    
+    // Accuracy check:
+    long diff = 0;
+    float correct_result = 0;
+    for (int j = 0; j < SAMPLE_COUNT; j++) {
+        fscanf(fptr_acc, "%f,", &correct_result);
+        if(abs(correct_result-result[j]) > epsilon) {
+             diff += 1;
+	     printf("%f", diff);
+        }
+    }
+
+    //fprintf(fptr, "%f,%f\n", epsilon, diff);
+    printf("Errors found with epsilon = %f: %f\n",epsilon,diff);
     printf("End Unthreaded Test\n");
+    
+    //fclose(fptr);
+
     return 0;
 }
 
